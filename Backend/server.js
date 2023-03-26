e5b53408524e1fc4ba21d7f36948ef1ee6b70478
phucnguyen02 <ndhp02@gmail.com>
2023-03-25 23:05:39 -0400

    - Started work on the MySQL database. - Haven't figured out how to connect to the MySQL server yet.

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,23 @@
 const express = require("express");
+const mysql = require("mysql");
 const app = express();
 const port = 3000;
+
+//Create database, currently haven't figured out a way to connect to the MySQL server 
+const db = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  password: '123456',
+  port: 3306
+})
+
+db.connect(function(err) {
+  if (err) {
+    return console.error('error: ' + err.message);
+  }
+
+  console.log('Connected to the MySQL server.');
+});
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -8,4 +25,13 @@ app.get("/", (req, res) => {
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
+});
+
+app.get("/createdb", (req, res) => {
+  let sql = 'CREATE DATABASE userDB';
+  db.query(sql, (err, result) => {
+    if(err) throw err;
+    console.log(result);
+    res.send("Database created.");
+  })
 });
