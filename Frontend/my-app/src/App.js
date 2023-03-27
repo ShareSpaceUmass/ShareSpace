0ca29bafb82513661f43c4f45f3106eb5b0ce66a
Alex Gornovoi <agornovoi@umass.edu>
2023-03-27 18:12:41 -0400

    Removed second BrowserRouter

--- Frontend/my-app/src/App.js
+++ Frontend/my-app/src/App.js
@@ -9,8 +9,7 @@ import LoginPage from './pages/Login';
 
 function App() {
   return (
-    <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-md w-full space-y-8">
+    <div >
         <BrowserRouter>
           <Routes>
             <Route path="/" element={<LoginPage />} />
@@ -18,7 +17,6 @@ function App() {
           </Routes>
         </BrowserRouter>
       </div>
-    </div>
   );
 }
 
