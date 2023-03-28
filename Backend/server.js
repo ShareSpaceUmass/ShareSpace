5f3ab5f57eb9d09487073170a0d8e81c49b7d991
phucnguyen02 <ndhp02@gmail.com>
2023-03-28 19:35:31 -0400

    1 - Successfully connected the Node server to the local MySQL server 2 - Created basic SQL queries for database, table creation, inserting/updating users, viewing all users

--- Backend/server.js
+++ Backend/server.js
@@ -1,37 +1,92 @@
 const express = require("express");
-const mysql = require("mysql");
+const mysql = require("mysql2");
 const app = express();
-const port = 3000;
+const port = '3000';
 
-//Create database, currently haven't figured out a way to connect to the MySQL server 
+//Connect
 const db = mysql.createConnection({
-  host: '127.0.0.1',
+  host: 'localhost',
   user: 'root',
-  password: '123456',
-  port: 3306
-})
+  password: '',
+  database: 'userDatabase'
+});
 
-db.connect(function(err) {
+db.connect((err) => {
   if (err) {
     return console.error('error: ' + err.message);
   }
-
   console.log('Connected to the MySQL server.');
 });
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+
+//Create database
+app.get("/createDatabase", (req, res) => {
+  let sql = 'CREATE DATABASE userDatabase';
+  db.query(sql, (err, result) => {
+    if(err) return console.error('error: ' + err.message);
+    console.log(result);
+    res.send("Database created!");
+  })
 });
+
+//Create table
+app.get('/createUserTable', (req, res) => {
+  let sql = 'CREATE TABLE users(id INT Primary KEY AUTO_INCREMENT, email VARCHAR(255), password CHAR(30), username CHAR(20), bio VARCHAR(255))';
+  db.query(sql, (err, result) => {
+    if(err) return console.error('error: ' + err.message);
+    console.log(result);
+    res.send("users table created!");
+  });
+});
+
+//Insert user into users table
+app.get('/setUser', (req, res) => {
+  let user = {email: "testUser@gmail.com", password: "123", username: "testUser", bio: "I love nothing"};
+  let sql = 'INSERT INTO users SET ?';
+  db.query(sql, user, (err, result) => {
+    if(err) return console.error('error: ' + err.message);
+    console.log(result);
+    res.send("user added!");
+  });
+});
+
+//Get user from users table
+app.get('/getUser/:userId', (req, res) => {
+  let sql = 'SELECT * FROM users WHERE id ='+ req.params.userId;
+  db.query(sql, (err, result) => {
+    if(err) return console.error('error: ' + err.message);
+    console.log(result);
+    res.send("user fetched!");
+  });
+});
+
+//Upadate user from users table
+app.get('/updateUser/:userId/:field/:value', (req, res) => {
+  let value = req.params.value;
+  let sql = 'UPDATE users SET ' + req.params.field + ' = ';
+  if(typeof(value) == 'string')
+    sql += '\'' + req.params.value + '\'' + ' WHERE id = ' + req.params.userId;
+  else
+    sql += req.params.value + ' WHERE id = '+ req.params.userId;
+
+  db.query(sql, (err, result) => {
+    if(err) return console.error('error: ' + err.message);
+    console.log(result);
+    res.send("user updated!");
+  });
+});
+
+//Get all users from user table
+app.get('/getAllUsers', (req, res) => {
+  let sql = 'SELECT * FROM users';
+  db.query(sql, (err, result) => {
+    if(err) return console.error('error: ' + err.message);
+    console.log(result);
+    res.send("users fetched!");
+  });
+});
+
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
-
-app.get("/createdb", (req, res) => {
-  let sql = 'CREATE DATABASE userDB';
-  db.query(sql, (err, result) => {
-    if(err) throw err;
-    console.log(result);
-    res.send("Database created.");
-  })
+    console.log(`Example app listening on port ${port}`);
 });
