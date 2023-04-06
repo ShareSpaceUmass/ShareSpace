967921b31ac1cc0be2d3c9f7f72bbc3ed3389099
rfrost2 <ryan.frost926@gmail.com>
2023-04-06 09:25:46 -0400

    fixed centering and added post request

--- Frontend/src/pages/Signup.js
+++ Frontend/src/pages/Signup.js
@@ -27,8 +27,21 @@ function SignupPage() {
     }
 
     const missingInfo = () => {
-      return (fName == '') || (lName == '') || (email == '') || (gender == '')
+      return (fName === '') || (lName === '') || (email === '') || (gender === '')
     }
+
+    const register = () => {
+      const user = {fName, lName, email, gender}
+
+      const response = fetch('http://localhost:4000/users/signup', {  
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json' },
+          body: JSON.stringify(user)
+      })
+      response.then((res) => res.json())
+          .then((data) => console.log(data))
+  }
 
     Aos.init({ duration: 1800, offset: 0});
     return (
@@ -65,14 +78,11 @@ function SignupPage() {
         >
             <Grid
             container
-            direction="row"
-            alignItems="center"
-            justifyContent="center"
             spacing={{xs:3, md:6}}
             marginTop={{xs:0, md:0}}
             sx={{
               maxHeight: { xs: 900, md: 200 },
-              maxWidth: { xs: 250, md: 1000 },
+              maxWidth: { xs: 324, md: 895 },
             }}
             >
                 <Grid item xs={12} md={6}>
@@ -87,7 +97,7 @@ function SignupPage() {
                     sx={{
                       width:400,
                       maxHeight: { xs: 200, md: 200 },
-                      maxWidth: { xs: 200, md: 400 },
+                      maxWidth: { xs: 300, md: 400 },
                     }}
                     />
                 </Grid>
@@ -103,7 +113,7 @@ function SignupPage() {
                     sx={{
                       width:400,
                       maxHeight: { xs: 200, md: 200 },
-                      maxWidth: { xs: 200, md: 400 },
+                      maxWidth: { xs: 300, md: 400 },
                     }}
                     />
                 </Grid>
@@ -124,7 +134,7 @@ function SignupPage() {
                     sx={{
                       width:400,
                       maxHeight: { xs: 200, md: 200 },
-                      maxWidth: { xs: 200, md: 400 },
+                      maxWidth: { xs: 300, md: 400 },
                     }}
                     />
                 </Grid>
@@ -133,7 +143,7 @@ function SignupPage() {
                     sx={{
                         width:400,
                         maxHeight: { xs: 200, md: 200 },
-                        maxWidth: { xs: 200, md: 400 },
+                        maxWidth: { xs: 300, md: 400 },
                     }}>
                     <InputLabel id="gender" 
                     sx={{
@@ -181,6 +191,7 @@ function SignupPage() {
                 <Button
                 variant="contained"
                 disabled={missingInfo() || emailError}
+                onClick={register}
                 color="secondary"
                 sx={{
                   width:250,
