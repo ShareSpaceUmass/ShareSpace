f009800eb122d4f695a83000fca8d2c1d05e23e9
Alex Gornovoi <agornovoi@umass.edu>
2023-04-09 16:56:57 -0400

    Fixed layout

--- Frontend/src/pages/Chat.js
+++ Frontend/src/pages/Chat.js
@@ -1,5 +1,6 @@
 import { Grid, Container, Box, Stack, Typography, IconButton, TextField, Divider } from "@mui/material"
 import SendIcon from '@mui/icons-material/Send';
+import Header from "../components/Header";
 import Aos from "aos";
 
 const borderStyles = {
@@ -14,42 +15,52 @@ const borderStyles = {
 function ChatPage() {
     Aos.init({ duration: 1000, offset: 0 });
     return (
-        <Box minHeight="100vh"
-            display="flex"
-            justifyContent="center"
-            alignItems="center"
-        >
-            <Container maxWidth="md">
-                <Grid container spacing={1}>
-                    <Grid md={3} data-aos="fade-up">
-                        <Stack minHeight="75vh" sx={borderStyles}>
-                            <Typography>Test</Typography>
-                        </Stack>
-                    </Grid>
-                    <Grid md={9} data-aos="fade-down">
-                        <Box minHeight="75vh" maxHeight="75vh" sx={borderStyles} display= "flex"
-      alignItems= "end" >
-                            <Box></Box>
-                            <Grid container>
-                                <Grid md={11}>
-                                    <TextField
-                                        fullWidth
-                                        id="outlined-basic"
-                                        variant="outlined"
-                                        multiline
-                                    />
+        <Stack>
+            <Header />
+            <Box
+                minHeight="80vh"
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+            >
+                <Container maxWidth="md">
+                    <Grid container spacing={1}>
+                        <Grid md={3} data-aos="fade-up">
+                            <Stack minHeight="75vh" sx={borderStyles}>
+                                <Typography>Test</Typography>
+                            </Stack>
+                        </Grid>
+                        <Grid md={9} data-aos="fade-down">
+                            <Stack
+                                minHeight="75vh"
+                                maxHeight="75vh"
+                                sx={borderStyles}
+                                display="flex"
+                                justifyContent="flex-end"
+                            >
+                                <Box></Box>
+                                <Divider />
+                                <Grid container>
+                                    <Grid md={11}>
+                                        <TextField
+                                            fullWidth
+                                            id="outlined-basic"
+                                            variant="outlined"
+                                            multiline
+                                        />
+                                    </Grid>
+                                    <Grid md={1}>
+                                        <IconButton sx={{ margin: "10px" }}>
+                                            <SendIcon></SendIcon>
+                                        </IconButton>
+                                    </Grid>
                                 </Grid>
-                                <Grid md={1}>
-                                    <IconButton sx={{margin:"10px"}}>
-                                        <SendIcon></SendIcon>
-                                    </IconButton>
-                                </Grid>
-                            </Grid>
-                        </Box>
+                            </Stack>
+                        </Grid>
                     </Grid>
-                </Grid>
-            </Container>
-        </Box>
+                </Container>
+            </Box>
+        </Stack>
     )
 }
 
