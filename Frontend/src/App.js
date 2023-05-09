7ec86eb320b5b8906a4a0598f4f745157804a991
alexgornovoi <alex.gornovoi@gmail.com>
2023-05-08 21:00:22 -0400

    added private routes

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -13,25 +13,45 @@ import PreferencePage from "./pages/Preferences";
 import ProfilePage from "./pages/Profile"
 import MatchPage from "./pages/Matches"
 import ChatPage from "./pages/Chat";
+import { RequireAuth } from 'react-auth-kit'
+import { AuthProvider } from 'react-auth-kit'
+
 
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LandingPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/signup" element={<SignupPage />} />
-          <Route path="/quiz" element={<QuizPage />} />
-          <Route path="/preferences" element={<PreferencePage />} />
-          <Route path="/profile" element={<ProfilePage />} />
-          <Route path="/matches" element={<MatchPage />} />
-          <Route path="/chat" element={<ChatPage />} />
-        </Routes>
-      </BrowserRouter>
+      <AuthProvider authType={'cookie'}
+        authName={'_auth'}
+        cookieDomain={window.location.hostname}
+        cookieSecure={window.location.protocol === "https:"}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<LandingPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/quiz" element={<RequireAuth loginPath={'/login'}>
+              <QuizPage />
+            </RequireAuth>} />
+            <Route path="/preferences" element={<RequireAuth loginPath={'/login'}>
+              <PreferencePage />
+            </RequireAuth>} />
+            <Route path="/profile" element={<RequireAuth loginPath={'/login'}>
+              <ProfilePage />
+            </RequireAuth>} />
+            <Route path="/matches" element={<RequireAuth loginPath={'/login'}>
+              <MatchPage />
+            </RequireAuth>} />
+            <Route path="/chat" element={<RequireAuth loginPath={'/login'}>
+              <ChatPage />
+            </RequireAuth>} />
+          </Routes>
+        </BrowserRouter>
+      </AuthProvider>
     </ThemeProvider>
   );
 }
+
+
 
 export default App;
