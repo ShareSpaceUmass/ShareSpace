a097845118ed3963d7ebf43f7ca3bead7bde5011
CalebTeeter <calstu99@gmail.com>
2023-05-09 15:36:11 -0400

    fix message route
    updated message routes file to have the necessary route

--- Backend/routes/messagesRoutes.js
+++ Backend/routes/messagesRoutes.js
@@ -1,3 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const {  } = require('../controllers/messagesController')
+const { markRead } = require('../controllers/messagesController')
+
+router.post('/markRead', markRead);
+
+module.exports = router;
